bf3f3652d56b36213db048045fd0dc193084f2b4
LHCPlayz <botesjovan@gmail.com>
2025-03-22 16:06:18 +0100

    Added panels and base quiz funtion

--- Assets/scripts/wizard.cs
+++ Assets/scripts/wizard.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class wizard : MonoBehaviour
 {
    
+    public GameObject Quiz1; // Assign the Quiz1 panel in the Inspector
+
     private void OnCollisionEnter(Collision collision)
     {
         // Check if the colliding object has the tag "Player"
         if (collision.gameObject.CompareTag("player"))
         {
-            Debug.Log("collide");
+            Quiz1.SetActive(true); // Unhide the Quiz1 panel
         }
     }
 }
