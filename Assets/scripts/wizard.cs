b62e84a535340b8f0288e65d91ef4d4986d85b1f
LHCPlayz <botesjovan@gmail.com>
2025-03-25 12:55:45 +0100

    Update to quiz
    Assigned bridge, wizard and quiz to the script. demo of this function works

--- Assets/scripts/wizard.cs
+++ Assets/scripts/wizard.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class wizard : MonoBehaviour
 {
    
     public GameObject Quiz1; // Assign the Quiz1 panel in the Inspector
+    public GameObject Bridge; // Assign the Bridge in the Inspector
+    public GameObject barrier; // Assign the barrier in the Inspector
+    public GameObject wizardchar; // Assign the wizard in the Inspector
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -11,6 +15,24 @@ public class wizard : MonoBehaviour
         if (collision.gameObject.CompareTag("player"))
         {
             Quiz1.SetActive(true); // Unhide the Quiz1 panel
+            
+
+
         }
+    }
+    public void OnTrueButtonClicked()
+    {
+        Quiz1.SetActive(false); // Hide the Quiz1 panel
+        Debug.Log("True button clicked!");
+        Bridge.SetActive(true); // Unhide the Bridge
+        barrier.SetActive(false); // Hide the barrier
+        wizardchar.SetActive(false); // Hide the wizard
+        
+    }
+
+    public void OnFalseButtonClicked()
+    {
+        Quiz1.SetActive(false); // Hide the Quiz1 panel
+        Debug.Log("False button clicked!");
     }
 }
